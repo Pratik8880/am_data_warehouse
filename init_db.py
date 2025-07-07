5d24be1326cb1ed7f051a22c8a52c41687137f07
Pratik8880 <pratikkedari27@gmail.com>
2025-07-07 15:02:38 +0530

    update removal unecessary

--- init_db.py
+++ init_db.py
@@ -3,4 +3,4 @@ from models import db
 
 with app.app_context():
     db.create_all()         # to create all tables from models.py
-    print("✅ Tables created successfully in the database.")
+    print("Tables created successfully in the database.")
